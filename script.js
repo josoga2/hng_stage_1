c75ffbbb2a74bda49384c53761d3a20036f435bc
Adewale Joseph OGUNLEYE <30349247+josoga2@users.noreply.github.com>
2024-07-01 04:23:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    function updateTime() {
+        const timeElement = document.getElementById('currentTimeUTC');
+        const now = new Date();
+        const timeString = now.toUTCString();
+        timeElement.textContent = `Current Time: ${timeString.split(" ")[4]} UTC`;
+    };
+    
+    function updateDay(){
+        const dayElement = document.getElementById('currentDay')
+        const day = new Date();
+        const dayString = day.toUTCString();
+        dayElement.textContent = `Current Day: ${dayString.split(" ").slice(0,4).join(" ")}`;
+    }
+
+    // Update the time immediately and then every second
+    updateTime();
+    setInterval(updateTime, 1000);
+
+    updateDay();
+    setInterval(updateDay, 1000000)
+});
